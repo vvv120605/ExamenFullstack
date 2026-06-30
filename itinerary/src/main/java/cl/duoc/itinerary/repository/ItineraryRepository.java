be3b06fde6ba2e2d0c044e335b05e9a5015b287e
SJ- Alumno <SJ-Alumno@duoc.acad>
2026-06-30 11:53:44 -0400

    commit repository listo

--- itinerary/src/main/java/cl/duoc/itinerary/repository/ItineraryRepository.java
+++ itinerary/src/main/java/cl/duoc/itinerary/repository/ItineraryRepository.java
@@ -4,6 +4,8 @@ import cl.duoc.itinerary.model.Itinerary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface ItineraryRepository extends JpaRepository<Itinerary, Long> {
+public interface ItineraryRepository extends JpaRepository<Itinerary, UUID> {
 }
