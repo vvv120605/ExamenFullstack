51ab39e913239e8c30351df10209b29917c13072
SJ- Alumno <SJ-Alumno@duoc.acad>
2026-06-30 11:01:07 -0400

    feat: crear microservicio itinerary base

---
+++ itinerary/src/main/java/cl/duoc/itinerary/ItineraryApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.itinerary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItineraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItineraryApplication.class, args);
+	}
+
+}
