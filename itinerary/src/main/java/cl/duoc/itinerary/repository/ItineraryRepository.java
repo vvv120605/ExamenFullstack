52dd3e42b54b464d8aa1ba306d6f65f1c92d4039
SJ- Alumno <SJ-Alumno@duoc.acad>
2026-06-30 12:00:39 -0400

    config creado

--- itinerary/src/main/java/cl/duoc/itinerary/repository/ItineraryRepository.java
+++ itinerary/src/main/java/cl/duoc/itinerary/repository/ItineraryRepository.java
@@ -1,9 +1,12 @@
 package cl.duoc.itinerary.repository;
 
 import cl.duoc.itinerary.model.Itinerary;
+
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ItineraryRepository extends JpaRepository<Itinerary, Long> {
+public interface ItineraryRepository extends JpaRepository<Itinerary, UUID> {
 }
